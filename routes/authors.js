728569dc8caa9e6fcfd215afd5b24587867218e7
unknown <shahin.abdullayev.89@gmail.com>
2021-03-06 13:07:30 +0400

    Initial Author Routes

---
+++ routes/authors.js
@@ -0,0 +1,58 @@
+const { query } = require('express');
+const express=require('express');
+const router=express.Router();
+const Author=require('../models/author');
+//All authors
+router.get('/', async function(req, res){
+    let searchOption={};
+    if(req.query.name != null && req.query.name !== ''){
+        searchOption.name=new RegExp(req.query.name, 'i');
+    }
+    try{
+        const authors=await Author.find(searchOption);
+        res.render("authors/index", {authors:authors, searchOptions: req.query});
+    }catch{
+        res.redirect('/');
+    }
+    
+});
+
+//New Author Route
+router.get('/new', function(req, res){
+    res.render('authors/new', {author: new Author()});
+});
+
+
+//New Author creation route
+router.post('/', async function(req, res){
+    const author=new Author({
+        name: req.body.name
+    });
+    try{
+        const newAuthor=await author.save();
+        //res.redirect(`authors/${newAuthor.id}`);
+        console.log(author.name);
+        res.redirect(`authors`);
+    }catch{
+        res.render('authors/new',{
+            author: author,
+            errorMessage: 'Error creating author'
+        });
+    }
+    /*author.save(function(err, newAuthor){
+        if(err){
+            res.render('authors/new',{
+                author: author,
+                errorMessage: 'Error creating author'
+            });
+        }else{
+            //res.redirect(`authors/${newAuthor.id}`);
+            console.log(author.name);
+            res.redirect(`authors`);
+            
+        }
+    });*/
+});
+
+
+module.exports=router;
--- server.js
+++ server.js
@@ -5,26 +5,29 @@ if(process.env.NODE_ENV !=='production'){
 const express=require('express');
 const app=express();
 const expressLayouts=require("express-ejs-layouts");
-
+const bodyParser=require('body-parser');
 
 //Routes
 const indexRouter=require('./routes/index');
+const authRouter=require('./routes/authors');
 
 app.set("view engine", "ejs");
 app.set("views", __dirname+"/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
-
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 //MongoDb connection
 const mongoose =require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
 const db=mongoose.connection;
 db.on('error', function(error){console.log(error)});
-db.once('open',function(){console.log("Connected....")});
+db.once('open',function(){console.log("Connected to MongoDB....")});
 
 app.use('/', indexRouter);
+app.use('/authors', authRouter);
+
 
 app.listen(process.env.PORT||3000, function(){
     console.log("Server run in port 3000");
